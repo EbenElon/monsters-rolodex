31e502dca9d1e9934a49659824ebf6517c5481f3
Eben <ebenezernarh83@gmail.com>
2019-10-27 10:27:08 +0000

    Updates

---
+++ precache-manifest.cd16797f3c88ead02682ba6115251c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c2735d082d32e020603e85460785e2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8d89d2ee3dc8c3269b9",
+    "url": "/monsters-rolodex/static/css/main.581764ad.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "b8d89d2ee3dc8c3269b9",
+    "url": "/monsters-rolodex/static/js/main.4d025ad3.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
